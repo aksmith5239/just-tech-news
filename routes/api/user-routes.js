7da770e4192ebe5c8aee7f652a0d91232b0dca8d
aksmith5239 <aklobby@gmail.com>
2020-10-21 13:28:37 -0600

    password verification completed

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -47,6 +47,27 @@ router.post('/', (req, res) => {
     });
 });
 
+router.post('/login', (req, res) => {
+  //http://localhost:3001/api/users/login
+  // expects {email: 'email@email.com', password: 'pass1234'}
+  User.findOne({
+    where: {
+      email: req.body.email
+    }
+  }).then(dbUserData => {
+    if(!dbUserData) {
+      res.status(400).json({message: "No user with that email address! "});
+      return;
+    }
+    const validPassword = dbUserData.checkPassword(req.body.password);
+    if (!validPassword) {
+      res.status(400).json({message: "Incorrect password!"});
+      return;
+    }
+    res.json({user: dbUserData, message: "You are logged in!"});
+  });
+});
+
 router.put('/:id', (req, res) => {
   // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
 
