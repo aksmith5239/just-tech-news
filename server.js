30b7b460bda6e4a78e7b777c41c8ba81d4337338
aksmith5239 <aklobby@gmail.com>
2020-10-26 14:49:17 -0600

    homepage view completed

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./controllers');
 const path = require('path');
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
