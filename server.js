abe5e55332b0368099e12bfaa16d7a07cef93f93
aksmith5239 <aklobby@gmail.com>
2020-10-29 11:19:28 -0600

    partials and helpers added

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const routes = require('./controllers');
 const path = require('path');
+const helpers = require('./utils/helpers');
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+const hbs = exphbs.create({helpers});
 const sequelize = require('./config/connection');
 
 const session = require('express-session');
@@ -21,6 +22,7 @@ const sess = {
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
